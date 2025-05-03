1c30b511aa337ec93c05ed6b1f658a805620208c
Quirence <shades.h4ndeir@gmail.com>
2025-05-04 00:24:42 +0800

    camelCase errors refactoring

--- src/main/java/edu/phystech/hw3/SetUtils.java
+++ src/main/java/edu/phystech/hw3/SetUtils.java
@@ -5,21 +5,21 @@ import java.util.Set;
 
 public class SetUtils {
 	public static <E> Set<E> union(Set<? extends E> s1, Set<? extends E> s2) {
-		Set<E> union_set = new HashSet<>(s1);
-		union_set.addAll(s2);
-		return union_set;
+		Set<E> unionSet = new HashSet<>(s1);
+		unionSet.addAll(s2);
+		return unionSet;
 	}
 
 	public static <E> Set<E> intersection(Set<? extends E> s1, Set<? extends E> s2) {
-		Set<E> intersection_set = new HashSet<>(s1);
-		intersection_set.retainAll(s2);
-		return intersection_set;
+		Set<E> intersectionSet = new HashSet<>(s1);
+		intersectionSet.retainAll(s2);
+		return intersectionSet;
 	}
 
 	public static <E> Set<E> difference(Set<? extends E> s1, Set<? extends E> s2) {
-		Set<E> difference_set = new HashSet<>(s1);
-		difference_set.removeAll(s2);
-		return difference_set;
+		Set<E> differenceSet = new HashSet<>(s1);
+		differenceSet.removeAll(s2);
+		return differenceSet;
 	}
 
 	public static <E> Set<E> symmetricDifference(Set<? extends E> s1, Set<? extends E> s2) {
