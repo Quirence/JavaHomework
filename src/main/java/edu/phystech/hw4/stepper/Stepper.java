c39f4458a9428ac1fcdb1c4cd4b8459aa49f7de1
Quirence <shades.h4ndeir@gmail.com>
2025-05-13 19:15:52 +0800

    hw4 completed

--- src/main/java/edu/phystech/hw4/stepper/Stepper.java
+++ src/main/java/edu/phystech/hw4/stepper/Stepper.java
@@ -3,25 +3,40 @@ package edu.phystech.hw4.stepper;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * @author kzlv4natoly
- */
-
 public class Stepper {
 
-    public enum Side {
-        LEFT, RIGHT
-    }
+	public enum Side {
+		LEFT, RIGHT
+	}
 
-    private final List<Side> history = new ArrayList<>();
-    private final Object lock = new Object();
-    private boolean isLeftTurn = true;
 
-    public void leftStep() {}
+	private final List<Side> history = new ArrayList<>();
+	private final Object lock = new Object();
+	private boolean isLeftTurn = true;
 
-    public void rightStep()  {}
+	public void leftStep() throws InterruptedException {
+		synchronized (lock) {
+			while (!isLeftTurn) {
+				lock.wait();
+			}
+			history.add(Side.LEFT);
+			isLeftTurn = false;
+			lock.notifyAll();
+		}
+	}
 
-    public List<Side> getHistory() {
-        return history;
-    }
+	public void rightStep() throws InterruptedException {
+		synchronized (lock) {
+			while (isLeftTurn) {
+				lock.wait();
+			}
+			history.add(Side.RIGHT);
+			isLeftTurn = true;
+			lock.notifyAll();
+		}
+	}
+
+	public List<Side> getHistory() {
+		return history;
+	}
 }
--- src/test/java/edu/phystech/hw4/ConcurrentCounterTest.java
+++ src/test/java/edu/phystech/hw4/ConcurrentCounterTest.java
@@ -7,7 +7,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -17,37 +16,43 @@ import org.junit.jupiter.api.Test;
 /**
  * @author kzlv4natoly
  */
-class ConcurrentCounter {
-    private long value = 0;
-    void increment() {}
 
-    long getValue() { return 0; }
+class ConcurrentCounter {
+	private long value = 0;
+
+	synchronized void increment() {
+		value += 1;
+	}
+
+	synchronized long getValue() {
+		return value;
+	}
 }
 
 
 public class ConcurrentCounterTest {
 
-    @Test
-    void justWorks() {
-        ConcurrentCounter concurrentCounter = new ConcurrentCounter();
-        IntStream.range(0, 100_000).forEach(i -> concurrentCounter.increment());
-        Assertions.assertEquals(100_000, concurrentCounter.getValue());
-    }
+	@Test void justWorks() {
+		ConcurrentCounter concurrentCounter = new ConcurrentCounter();
+		IntStream.range(0, 100_000).forEach(i -> concurrentCounter.increment());
+		Assertions.assertEquals(100_000, concurrentCounter.getValue());
+	}
 
 
-    @Test
-    void worksWithConcurrentIncrement() throws InterruptedException {
-        ConcurrentCounter concurrentCounter = new ConcurrentCounter();
-        ExecutorService executorService = Executors.newFixedThreadPool(10);
-        List<Callable<Integer>> callableList = IntStream.range(0, 100_000).mapToObj(i -> (Callable<Integer>) () -> {
-                concurrentCounter.increment();
-                return 0;
-        }).toList();
-        executorService.invokeAll(callableList).forEach(f -> {
-            try {
-                f.get();
-            } catch (Exception e) {}
-        });
-        Assertions.assertEquals(100_000, concurrentCounter.getValue());
-    }
+	@Test void worksWithConcurrentIncrement() throws InterruptedException {
+		ConcurrentCounter concurrentCounter = new ConcurrentCounter();
+		ExecutorService executorService = Executors.newFixedThreadPool(10);
+		List<Callable<Integer>> callableList =
+			IntStream.range(0, 100_000).mapToObj(i -> (Callable<Integer>) () -> {
+				concurrentCounter.increment();
+				return 0;
+			}).toList();
+		executorService.invokeAll(callableList).forEach(f -> {
+			try {
+				f.get();
+			} catch (Exception e) {
+			}
+		});
+		Assertions.assertEquals(100_000, concurrentCounter.getValue());
+	}
 }
